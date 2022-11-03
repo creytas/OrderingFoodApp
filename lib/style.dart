9eb9c274b442b4ab4c3e0ba01a128df58868218d
Randy Buhendwa <creytas@gmail.com>
2022-11-03 13:22:47 +0100

    feat: create an appbar

---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primary =  Color(0xff50c9ff);
+const Color success =  Color(0xff00e582);
+const Color black =  Color(0xff4d4d4d);
+const Color button =  Color(0xff00d672);
+const Color btnAlt = Color.fromARGB(255, 3, 51, 90);
