dd5a003d6d20cbfd84452da397b63e96ad825909
Randy Buhendwa <creytas@gmail.com>
2022-11-04 16:07:13 +0100

    feat: create hotel page

---
+++ lib/hotel_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HotelPage extends StatelessWidget {
+  const HotelPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
